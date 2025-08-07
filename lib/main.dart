89cf9aeca18d5b84fbba3f18cc8ed398935a81e0
suwimon2004 <pangsuwimonn@gmail.com>
2025-08-07 15:35:22 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> items = List.generate(50, (index) => "Item ${index + 1}");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(itemCount: items.length,
+      itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+            subtitle: Text("Subtitle for item ${index + 1}"), 
+            onTap: () {
+              print(items[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
